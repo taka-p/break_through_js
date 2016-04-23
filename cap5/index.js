524ef40a1941d20cc41c4eb512a3e310778e3cc0
TakashiSato <takashisato.235711@gmail.com>
2016-04-23 19:50:51 +0900

    css3 animationによるアニメーション、deferredでanimaionEndをラップ

--- cap5/index.js
+++ cap5/index.js
@@ -4,24 +4,6 @@
 
     var $pages;
 
-    function urlChangeHandler() {
-        var pageid = parseUrl(location.hash);
-        $pages.filter(':visible')
-            .fadeOut()
-            .promise()
-            .then(function () {
-                $pages.hide()
-                    .detach()
-                    .filter('.page' + pageid)
-                    .appendTo('article')
-                    .fadeIn();
-            });
-    }
-
-    function parseUrl(url) {
-        return url.slice(1) || 1;
-    }
-
     function init() {
         $pages = $('[data-role=page]').detach();
         $(window)
@@ -31,4 +13,47 @@
 
     init();
 
+    function urlChangeHandler() {
+        var pageid = parseUrl(location.hash);
+        var $prevPage = $pages.filter(':visible');
+        var $nextPage = $pages.filter('.page' + pageid);
+
+        animEnd(
+            $prevPage.addClass('page-leave')
+        ).then(function () {
+
+            $pages
+                .detach()
+                .removeClass('page-leave');
+
+            return animEnd(
+                $nextPage
+                    .appendTo('article')
+                    .addClass('page-enter')
+            );
+        }).then(function () {
+           $nextPage.removeClass('page-enter');
+        });
+    }
+
+    function parseUrl(url) {
+        return url.slice(1) || 1;
+    }
+
+    function animEnd($el) {
+        var dfd = new $.Deferred,
+            callback = function () { dfd.resolve($el); };
+
+        if ($el.length === 0) {
+            dfd.resolve();
+            return dfd;
+        }
+
+        $el.on("webkitAnimationEnd", callback);
+        dfd.done(function () {
+            $el.off("webkitAnimationEnd", callback);
+        });
+
+        return dfd;
+    }
 })();
